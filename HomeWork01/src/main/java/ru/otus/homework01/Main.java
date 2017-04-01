5f58c9684613f8a9314a6251f7833622dbc4dd32
stvort <stvort@mail.ru>
2017-04-01 18:46:39 +0400

    first commit

---
+++ HomeWork01/src/main/java/ru/otus/homework01/Main.java
@@ -0,0 +1,8 @@
+package ru.otus.homework01;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hallo otus world!!!");
+        Thread.sleep(3000);
+    }
+}
